b7b0c00086994b4d77296dcbb71a7d8e57f58bce
odhiambo-ed <odhiambo.edward111@gmail.com>
2025-02-18 23:04:58 +0300

    add index js code

---
+++ conditions/ellipsis/index.js
@@ -0,0 +1,8 @@
+import { getDescription } from "./ellipsis.js";
+
+const input = document.querySelector("#input");
+const output = document.querySelector("#output");
+
+input.addEventListener("input", (event) => {
+    output.textContent = getDescription(event.currentTarget.value);
+});
