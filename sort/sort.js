acaaaae7df392e22017eedd95712f9bd4ae46311
odhiambo-ed <odhiambo.edward111@gmail.com>
2025-02-19 11:26:56 +0300

    add toSorted function

--- sort/sort.js
+++ sort/sort.js
@@ -1,4 +1,16 @@
 const numbers = [25, 3, 100, 7];
+// Sort numbers without compare function
 numbers.sort();
+console.log(numbers); // Output: [100, 25, 3, 7] ❌ (Incorrect order)
 
-console.log(numbers);
+// Compare numbers with compare function
+sortedNumbers = numbers.sort((a, b) => a - b);
+console.log(numbers); // Output: [3, 7, 25, 100] (Original array is modified)
+console.log(sortedNumbers);
+
+// Fix the mutation using toSorted
+secondSortedNumbers = numbers.toSorted((a, b) => a - b);
+console.log(numbers); // Output: [25, 3, 100, 7] (Original array is not modified)
+console.log(secondSortedNumbers); // Output: [3, 7, 25, 100] (Correct order)
+
+
