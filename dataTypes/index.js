95a58142d2d3c36979b75100b5dc732126830d8c
odhiambo-ed <odhiambo.edward111@gmail.com>
2025-03-27 10:51:50 +0300

    add datatypes

---
+++ dataTypes/index.js
@@ -0,0 +1,20 @@
+let text = "Hello Tash";
+let num = 20;
+let isTrue = false;
+let nothing = null;
+let missing;
+let car = {
+    make: "Benz",
+    model: "E250",
+    year: 2021
+}
+
+let cars = ["Benz", "Ford", "Mazda", "Range Rover"]
+
+console.log(typeof text);
+console.log(typeof num);
+console.log(typeof isTrue);
+console.log(typeof nothing);
+console.log(typeof missing);
+console.log(typeof car);
+console.log(typeof cars);
